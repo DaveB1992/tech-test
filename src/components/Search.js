a9d06c535bc020ada462b1e7446ee914cb672db1
Dave Bratby <dbratby1992@gmail.com>
2023-07-02 19:09:39 +0100

    created the handleSubmit event

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react"
 import "../styles/search.css"
+import getImages from "../requests/getImages"
 
 const Search = () => {
     const [value, setValue] = useState()
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        getImages(value)
+    }
+
     return (
-        <>
-        <form className="search-form">
+        <div className="search">
+        <form className="search-form" on-onSubmit={handleSubmit}>
         <input 
         className="search-input" 
         type="text"
         onChange={(e) => setValue(e.target.value)}/>
         <button className="search-btn" type="submit">Go!</button>
         </form>
-        </>
+    </div>
     )
 }
 
