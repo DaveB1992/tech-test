ba45ba1ba1ff3f3d4ec38821c8488158605bea65
Dave Bratby <dbratby1992@gmail.com>
2023-07-03 19:48:58 +0100

    added the get request to return just the images and filter out aything else

--- src/requests/getImages.js
+++ src/requests/getImages.js
@@ -7,10 +7,12 @@ const getImages = (query) => {
         return axios
         .get(`https://images-api.nasa.gov/search?q=${query}`)
         .then((response) => {
-            console.log(response)
+            const imageResults = response.data.collection.items;
+            const parsedImages = imageResults.filter((object) => object.data[0].media_type === "image");
+            return parsedImages.map(image => image.links[0].href)
         })
-        .catch((err) => {
-            console.log(err)
+        .catch((error) => {
+            console.log(error)
         })
     }
 }
